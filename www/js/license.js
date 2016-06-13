4c409eaf7b97b467dd71c2e381718eb5167a3a1d
mobilesupport <cheehaung@Chees-MacBook-Air.local>
2016-06-13 12:41:15 +0800

    update

--- www/js/license.js
+++ www/js/license.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        getvalue();
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
